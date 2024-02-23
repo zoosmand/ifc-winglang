1da6167c5b931947ce5664bc083938433c4a34c2
Dmitry Slobodchikov <d.slobodchikov@intraclear.com>
2024-02-23 11:52:42 +0200

    Make jest working

---
+++ core/makeGreeting.test.ts
@@ -0,0 +1,15 @@
+/**
+* Test Core Greeting.
+*/
+
+import { expect, it, describe } from '@jest/globals';
+import { makeGreeting } from './makeGreeting';
+
+describe('makeGreeting', () => {
+    it('Should return a greeting with the provided name', () => {
+        const name = "World!";
+        const expected = 'Hello, World!';
+        const result = makeGreeting(name);
+        expect(result).toBe(expected);
+    });
+});
---
+++ core/makeGreeting.ts
@@ -0,0 +1,7 @@
+/**
+* Core Greeting.
+*/
+
+export function makeGreeting(name: string): string{
+    return "Hello, " + name;
+};
